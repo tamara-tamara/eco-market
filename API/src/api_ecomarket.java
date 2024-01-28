62dff2f95e529df8e063551d73cde48bb0e93d8a
Tamara Alimkanova <alimkanova_t@auca.kg>
2024-01-28 17:15:40 +0600

    Transfer to personal repo

--- API/src/api_ecomarket.java
+++ API/src/api_ecomarket.java
@@ -1,4 +1,38 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class api_ecomarket {
-    //watch videos about creating api
-    // types and differences
+    private static final int PORT = 8080;
+
+    // IOException могут сгенерировать методы create() и bind(...)
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create();
+
+        httpServer.bind(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/hello", new HelloHandler());
+        httpServer.start(); // запускаем сервер
+
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            System.out.println("Началась обработка /hello запроса от клиента.");
+
+            String response = "Hey! Glad to see you on our server.";
+            httpExchange.sendResponseHeaders(200, 0);
+
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
 }
